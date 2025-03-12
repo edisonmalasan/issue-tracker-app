e25ad8e5e5f27baf6fb48a2b512ffe619117291d
Eruki <malasanedison1@gmail.com>
2025-03-13 00:50:29 +0800

    make use of classnames for easy string manipulation

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
     {
       label: "Dashboard",
-      href: "/Dashboard",
+      href: "/",
     },
     {
       label: "Issues",
@@ -22,7 +29,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
