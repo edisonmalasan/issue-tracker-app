92c4061d539f3003c21d2294a75a314db68909d9
Eruki <malasanedison1@gmail.com>
2025-03-13 00:33:58 +0800

    made links dynamic

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,16 +3,31 @@ import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "/Dashboard",
+    },
+    {
+      label: "Issues",
+      href: "/issues",
+    },
+  ];
   return (
-    <nav className="flex space-between space-x-6 p-4 border-b mb-5 px-5 h-14 items-center bg-gray-500 text-black">
-      <Link href="/">Logo</Link>
+    <nav className="flex space-between space-x-6 p-4 border-b mb-5 px-5 h-14 items-center text-black">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
