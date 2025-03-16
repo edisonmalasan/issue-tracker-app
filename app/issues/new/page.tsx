0a3dab2277f9650d5ae2787290990b7c60e94fdf
Eruki <malasanedison1@gmail.com>
2025-03-16 16:08:36 +0800

    added MDE

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssuePage = () => {
   return (
@@ -14,7 +15,7 @@ const NewIssuePage = () => {
           />
         </TextField.Slot>
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit New Issue</Button>
     </div>
   );
