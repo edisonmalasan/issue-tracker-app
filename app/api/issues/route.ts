d6243410419f8adb5fcbb13b8798fba34f3f3dbe
Eruki <malasanedison1@gmail.com>
2025-03-16 19:07:04 +0800

    server side validation for submitting form

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
 
     if (!validation.success) 
-        return NextResponse.json(validation.error.errors, { status: 400 }); // client error 
+        return NextResponse.json(validation.error.format(), { status: 400 }); // client error 
 
     const newIssue = await prisma.issue.create({
         data: { title: body.title, description: body.description}
